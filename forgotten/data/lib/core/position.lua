27dafa8585281b245af2fc1aee24be041678feed
Marcelo Petrucelli <m2petrucelli@gmail.com>
2018-01-04 17:49:08 -0200

    TFS rodando na versão 64x

---
+++ forgotten/data/lib/core/position.lua
@@ -0,0 +1,48 @@
+Position.directionOffset = {
+	[DIRECTION_NORTH] = {x = 0, y = -1},
+	[DIRECTION_EAST] = {x = 1, y = 0},
+	[DIRECTION_SOUTH] = {x = 0, y = 1},
+	[DIRECTION_WEST] = {x = -1, y = 0},
+	[DIRECTION_SOUTHWEST] = {x = -1, y = 1},
+	[DIRECTION_SOUTHEAST] = {x = 1, y = 1},
+	[DIRECTION_NORTHWEST] = {x = -1, y = -1},
+	[DIRECTION_NORTHEAST] = {x = 1, y = -1}
+}
+
+function Position:getNextPosition(direction, steps)
+	local offset = Position.directionOffset[direction]
+	if offset then
+		steps = steps or 1
+		self.x = self.x + offset.x * steps
+		self.y = self.y + offset.y * steps
+	end
+end
+
+function Position:moveUpstairs()
+	local swap = function (lhs, rhs)
+		lhs.x, rhs.x = rhs.x, lhs.x
+		lhs.y, rhs.y = rhs.y, lhs.y
+		lhs.z, rhs.z = rhs.z, lhs.z
+	end
+
+	self.z = self.z - 1
+
+	local defaultPosition = self + Position.directionOffset[DIRECTION_SOUTH]
+	local toTile = Tile(defaultPosition)
+	if not toTile or not toTile:isWalkable() then
+		for direction = DIRECTION_NORTH, DIRECTION_NORTHEAST do
+			if direction == DIRECTION_SOUTH then
+				direction = DIRECTION_WEST
+			end
+
+			local position = self + Position.directionOffset[direction]
+			toTile = Tile(position)
+			if toTile and toTile:isWalkable() then
+				swap(self, position)
+				return self
+			end
+		end
+	end
+	swap(self, defaultPosition)
+	return self
+end
